d8e3e54bcd0a8d6c8362dc9cd3de9b6370d23c7c
AndreFoulem <timmycasual@icloud.com>
2023-03-10 14:05:00 -0800

    Add event switch

--- SwiftAlgoDocs/Persistence.swift
+++ SwiftAlgoDocs/Persistence.swift
@@ -6,9 +6,12 @@
 //
 
 import CoreData
+import Combine
+import CloudKit
 
-struct PersistenceController {
+class PersistenceController {
     static let shared = PersistenceController()
+  
 
     //MARK: - SwiftUI Preview Helper
     static var preview: PersistenceController = {
@@ -22,21 +25,65 @@ struct PersistenceController {
     }()
 
     let container: NSPersistentCloudKitContainer
+    var subscriptions = Set<AnyCancellable>()
 
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "SwiftAlgoDocs")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+       //-* ELSE persistent
+        } else {
+          #if DEBUG
+          setupSchemaSync()
+          #endif
+          
         }
+      
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-             
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
       //-> ICLOUD LINE
         container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
     }
-  
+}
 
+extension PersistenceController {
+  
+  func setupSchemaSync() {
+    let options = NSPersistentCloudKitContainerSchemaInitializationOptions()
+    
+    do {
+      try container.initializeCloudKitSchema(options: options)
+    } catch {
+      print("Could kit initializeCloudKitSchema + options fail")
+    }
+  }
+  
+  func checkSyncStatus() {
+    NotificationCenter.default.publisher(for: NSPersistentCloudKitContainer.eventChangedNotification)
+      .sink { notification in
+        
+        guard let event = notification.userInfo?[NSPersistentCloudKitContainer.eventNotificationUserInfoKey] as? NSPersistentCloudKitContainer.Event else { return }
+        
+        if event.endDate == nil {
+          print("")
+        } else {
+          switch event.type {
+            case .setup: print("cloudkit event finished")
+            case .import: print("cloudkit event import")
+            case .export: print("Cloudkit event export")
+            @unknown default:
+              print("cloudkitevent - added a new event type")
+          }
+        }
+        
+      }
+      .store(in: &subscriptions)
+  }
+  
+  
+  
 }
