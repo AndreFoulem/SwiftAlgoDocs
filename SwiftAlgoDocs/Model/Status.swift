06c41f254615f34d9a68f8410087254f4fc8f38d
AndreFoulem <timmycasual@icloud.com>
2023-03-14 09:46:28 -0700

    Implement Status enum

--- SwiftAlgoDocs/Model/Status.swift
+++ SwiftAlgoDocs/Model/Status.swift
@@ -7,3 +7,12 @@
 
 import Foundation
 
+enum Status: String, Identifiable, CaseIterable {
+  case draft = "Draft"
+  case review = "Review"
+  case archived = "Archived"
+  
+  var id: String {
+    return self.rawValue
+  }
+}
