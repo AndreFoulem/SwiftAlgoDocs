135535e11b9f3e35918db0340bde3c8e4e59d3a8
AndreFoulem <timmycasual@icloud.com>
2023-03-14 09:55:34 -0700

    Add Picker(Status)

--- SwiftAlgoDocs/NoteDetailView.swift
+++ SwiftAlgoDocs/NoteDetailView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct NoteDetailView: View {
   @ObservedObject var note: Note
+  
+  @State var status: Status = .draft
 
     var body: some View {
 //      let textBinding = Binding(
 //        get: { note.title ?? "" },
 //        set: { note.title = $0 }
 //      )
+      
    
       return VStack(spacing:20) {
         Text("Note Detail View")
@@ -23,6 +26,18 @@ struct NoteDetailView: View {
           Text("Title: ")
           Text(note.title)
         }
+        
+        Picker(selection: $status) {
+          ForEach(Status.allCases) { status in
+            Text(status.rawValue)
+            //add a .tag to have the picker selectable on rawValue
+              .tag(status)
+          }
+        } label: {
+          Text("Note's Status")
+        }
+        .pickerStyle(.segmented)
+
         
         TextField("title", text: $note.title)
           .textFieldStyle(.roundedBorder)
