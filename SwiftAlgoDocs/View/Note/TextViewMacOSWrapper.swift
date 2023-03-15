f259051352f5cfb06d917609e68308000a072642
AndreFoulem <timmycasual@icloud.com>
2023-03-15 10:05:29 -0700

    Add file

---
+++ SwiftAlgoDocs/View/Note/TextViewMacOSWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  TextViewMacOSWrapper.swift
+//  SwiftAlgoDocs
+//
+//  Created by AndreMacBook on 2023-03-15.
+//
+
+import SwiftUI
+
+struct TextViewMacOSWrapper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct TextViewMacOSWrapper_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TextViewMacOSWrapper()
+//    }
+//}
