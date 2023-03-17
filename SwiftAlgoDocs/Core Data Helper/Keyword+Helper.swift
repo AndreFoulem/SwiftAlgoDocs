e47691bff83900859b38db7cf6d45e4282aaf374
AndreFoulem <timmycasual@icloud.com>
2023-03-17 11:45:46 -0700

    Add colorHex property extension getter

--- SwiftAlgoDocs/Core Data Helper/Keyword+Helper.swift
+++ SwiftAlgoDocs/Core Data Helper/Keyword+Helper.swift
@@ -7,15 +7,22 @@
 
 import SwiftUI
 
-//extension Keyword {
-//
-//  var colorHex: Color {
-//    get {  }
-//    set {
-//      // newValue
-//
-//
-//    }
-//  }
-//
-//}
+extension Keyword {
+
+  var colorHex: Color {
+    get {
+      if let colorHexValue = colorHex_,
+         let color = Color(hex: colorHexValue) {
+        return color
+      } else {
+        return Color.black
+      }
+    }
+    set {
+      // newValue
+
+
+    }
+  }
+
+}
