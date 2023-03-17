bbfabc3024158b801edf6f86afdef287eefabfc8
AndreFoulem <timmycasual@icloud.com>
2023-03-17 10:46:12 -0700

    Add Color extension for Hex Handling

---
+++ SwiftAlgoDocs/Core Data Helper/Keyword+Helper.swift
@@ -0,0 +1,21 @@
+//
+//  Keyword+Helper.swift
+//  SwiftAlgoDocs
+//
+//  Created by AndreMacBook on 2023-03-17.
+//
+
+import SwiftUI
+
+extension Keyword {
+  
+  var colorHex: Color {
+    get {  }
+    set {
+      // newValue
+    
+      
+    }
+  }
+  
+}
